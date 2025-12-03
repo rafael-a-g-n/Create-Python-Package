f621543dea25dbd1c5a7b199718f85380a715bc6
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-12-03 13:44:08 +0000

    Implemented basic arithmetic functions (square, double, add)

---
+++ mymath/basic.py
@@ -0,0 +1,25 @@
+"""
+A basic math module.
+This module provides simple mathematical functions.
+"""
+
+
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+
+def add(a, b):
+    """
+    This function returns the sum of given numbers
+    """
+    return a + b
